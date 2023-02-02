74699a08396c4cd88e37ace1d3919f8c54726ae4
Cole <cole.smith.code@gmail.com>
2023-02-02 16:44:22 -0700

    making components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import 'nav';
+
+
+function nav(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <p>
+
+      <a type='button'>Cats</a>
+
+      <a type='button'>Dogs</a>
+
+      <a type='button'>Birds</a>
+
+    </p>
   );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 export default App;
